16f9b6fd7cca26e93fa6b88ce6ec8f8261bef40f
JJ <87551031+RandomPerson1236@users.noreply.github.com>
2023-02-07 00:01:27 +0000

    Updated syntax and html further

---
+++ PrankSite/PrankSite/example.js
@@ -0,0 +1,52 @@
+$(()=>{
+    let js_main = document.querySelectorAll('.js-m');
+    const types_1 = [
+        'function', 'let',
+        'const', 'var',
+        'async', 'for',
+        'while', 'do',
+        'if', 'else'
+    ];
+    const types_2 = [
+        'split', 'replace',
+        'forEach', 'querySelectorAll',
+        'querySelector',
+        'getElementById', 'getElementbyClassName',
+        'getElementByTagName', 'join'
+    ];
+
+    let data = null;
+
+    function trigger_syntax(){
+        js_main.forEach(el => {
+            for (let i = 0; i < el.children.length; i++){
+                if (el.children[i].tagName = 'code'){
+                    data = el.children[i].innerHTML;
+
+                    if (el.classList.contains('fill-spaces')){
+                        data = data.replace(/  /g, '<span class="token-empty-space">•</span>')
+                    }
+
+                    data = data.replace(/'(.*?)'/g, '<span class="token-string">&apos;$1&apos;</span>');
+
+                    types_1.forEach(type1 =>{
+
+                        let reg = new RegExp(type1, 'g')
+                        data = data.replace(reg, `<span class="token-tp1">${type1}</span>`);
+
+                    });
+
+                    types_2.forEach(type2 => {
+
+                        let reg = new RegExp(type2, 'g')
+                        data = data.replace(reg, `<span class="token-tp2">${type2}</span>`);
+
+                    });
+
+                    el.children[i].innerHTML = data;
+                }
+            }
+        });
+    }
+    trigger_syntax();
+})
